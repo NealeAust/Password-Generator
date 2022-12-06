92374d86c5912363bee740a889b20b40db064f84
NealeAust <nealechapman@optushome.com.au>
2022-12-07 08:16:21 +1100

    updated commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,47 @@
 // Assignment code here
 
 // Declare variables
-function generatePassword(){
+
   var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+",",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", ']', '^', '_', "`", "{", "|", "}", "~"];
   
 
-  var characters = [...lowerCaseLetters, ...upperCaseLetters, ...numbers, ...symbols,]
+  
+  var length = window.prompt("Enter password length between 8 and 128 characters:", " ");
+  var number = ParseInt(length, radix);
+  
+  if(number >= 8 && number <=128) {
+
+    
+  var chartype1 = window.confirm(“Do you want to include lower case letters?”);
+  var chartype2 = window.confirm(“Do you want to include upper case letters?”);
+  var chartype3 = window.confirm(“Do you want to include numbers?”);
+  var chartype4 = window.confirm(“Do you want to include symbols”?);
+  
+  Else (
+ 
+  window.alert(“Password must be between 8 and 128 characters");
+   
+  
+ 
+    
+if( chartype1 === true &&|| chartype2 === true &&|| chartype3 === true  &&|| chartype-4 === true); 
+  
+  
 
 
 // Creating random password
 
 function generatePassword () {
+  prompt("Please enter a length for your password between 8 and 128");
+  prompt("Please indicate if you the charcter types to include")
+  alert("Length ")
+  prompt("Do I want to use " + lowercaseLetters + upperCaseLetter)
   var password = "";
-  var chosen = [];
-  chosen.length = min="8", max="128";
   for (let i = 0; i < password.length; i++) {
-  password += characters.charAt(
+ 
   var index= Math.floor(Math.random() * characters.length);
   result.innerText = chosen.join;
   return password.join ('');
