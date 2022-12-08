2966df8ac1a29681bc10214d4e29c49dfdcc9c1c
NealeAust <nealechapman@optushome.com.au>
2022-12-08 17:39:17 +1100

    updated content

--- README.md
+++ README.md
@@ -1,6 +1,57 @@
-<<<<<<< HEAD
-# Password Generator Starter Code
-=======
 # Password-Generator
-Password Generator, 8&lt;=128 characters. To include lowercase, uppercase, numeric and/or special characters.
->>>>>>> b7857ee38b5394909a93e640884eee3126ad659f
+
+## Project Title
+
+To build a random password generator that automically generates a strong and unpredictable password in accordance with parameters entered by the user.  
+
+## Password Generator Webpage
+
+The Password Generator webpage is a functional and easy to use random password generator that has been built using HTML, CSS and JavaScript
+
+### Functionality
+
+Upon clicking on the 'Generate Password' button to start the process a prompt appears for the user to enter the length of their password, between 8 and 128 characters. Once entered and validated, a series of four prompts will appear for the user to select the character types to be included: 
+- Lower case letters (a, b, c, d...)
+- Upper case letters (A, B, C, D...)
+- Numbers (1,2,3,4...)
+- Symbols (@, &, /, !...)
+
+The application has built in business rules so if the length is outside the specified range an alert is displayed. When the user clicks the OK button, the process is terminated. The business rule for character types is that 'at least one character type’ must be selected, otherwise an alert is displayed. When the user clicks the OK button, the process is terminated. 
+
+If the criteria entered is within the required parameters, the application will immediately return and display a random password. The length and character types included determine how strong the password is.
+
+## Project Description
+
+The objective of this project was to build a random password generator using Javascript.
+
+
+
+## Installation
+
+N/A
+
+## Usage
+
+
+
+## Deployed Application and Screenshot
+
+##### Link to the deployed application:
+
+https://github.com/NealeAust/Password-Generator
+
+
+##### Screenshot
+
+![image](https://user-images.githubusercontent.com/115671306/206374856-605d4f49-d591-4fdf-8a29-d247f97da785.png)
+
+
+
+## Credits
+
+N/A
+
+
+## License
+
+https://github.com/NealeAust/Password-Generator/blob/main/LICENSE
--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Assignment code here
 
 // Declare variables
-
-
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -30,8 +28,8 @@ function generatePassword() {
 
     window.alert('Password must be between 8 and 128 characters.')
     clear();
-  }
 
+  }
 
   if (chartype1 === false && chartype2 === false && chartype3 === false && chartype4 === false) {
 
@@ -56,8 +54,8 @@ function generatePassword() {
     console.log(choices);
   }
   
-  // Selects required number of characters from selection list created above
-  // Formulates and returns a password based on criteria provided by user
+// Selects required number of characters from selection list created above
+// Formulates and returns a password based on criteria provided by user
 var password = "";
   for (let i = 0; i < number; i++) {
      var index = Math.floor(Math.random() * choices.length);
@@ -70,72 +68,7 @@ var password = "";
 
 }
 
-// Display password on screen
-
-  var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+",",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", ']', '^', '_', "`", "{", "|", "}", "~"];
-  
-
-  function generatePassword () {
-
-  var choices = []
-
-  var length = window.prompt('Enter password length between 8 and 128 characters:', '');
-  var number = parseInt (length);
-  
-  if (number >= 8 && number <=128) {
-
-  var chartype1 = window.confirm('Do you want to include lower case letters?'); 
-  var chartype2 = window.confirm('Do you want to include upper case letters?'); 
-  var chartype3 = window.confirm('Do you want to include numbers?'); 
-  var chartype4 = window.confirm('Do you want to include symbols?');
-  
-  } else {
- 
-  window.alert('Password must be between 8 and 128 characters'); 
-
-}
-  
-
-if  (chartype1 === false && chartype2 === false && chartype3 === false  && chartype4 === false) {
-  
-  window.alert('Must click on OK to choose at least one character type'); 
-}
-if (chartype1 === true){           
-  choices =choices.concat (lowerCaseLetters);
-}
- if (chartype2 === true){
-  choices = choices.concat (upperCaseLetters);
-}
-if (chartype3 === true){
-  choices = choices.concat (numbers);
- }
- if (chartype4 === true){
-  choices = choices.concat (symbols);
- }
-
-console.log(choices);
-
-
-// Creating random password
-
-
-  var password = "";
-  for (let i = 0; i < number; i++) {
-   var index = Math.floor(Math.random() * choices.length);
-    password += choices[index];
-
-  }
-  return password;
-  console.log(password);
- 
-  }
-
-
 //Generate show password on screen
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
